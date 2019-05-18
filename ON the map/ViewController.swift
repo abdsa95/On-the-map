ea24ec5338b22e822fc0de7dd754aa8c8fea28c6
abdsa95 <abdsa.799@hotmail.com>
2019-05-18 06:44:39 +0300

    Initial Commit

---
+++ ON the map/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ON the map
+//
+//  Created by Abdualziz Aljuaid on 18/05/2019.
+//  Copyright © 2019 Abdualziz Aljuaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
